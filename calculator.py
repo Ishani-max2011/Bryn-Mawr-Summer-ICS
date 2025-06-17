9ed943b773def8ad4f76186e7ab3b1fd7f22face
Ishani-max2011 <gangulyi@brynmawrschool.org>
2025-06-17 10:43:43 -0400

    add new file

---
+++ calculator.py
@@ -0,0 +1,30 @@
+num1 = 2
+num2 = 3
+num3 = 4
+num4 = 5
+num5 = 6
+num6 = 7
+num7 = 8
+num8 = 9
+num9 = 10
+num10 = 11
+
+sum = num1 + num2
+print("Sum of num1 and num2")
+print(sum)
+
+mult = num3 * num4
+print("Multiplication of num3 and num4")
+print(mult)
+
+div = num5 / num6
+print("Division of num5 and num6")
+print(div)
+
+sub = num8 - num7
+print("Subtraction of num7 and num8")
+print(sub)
+
+mod = num10 % num9
+print("Modulus of num10 and num9")
+print(mod)
---
+++ test.py
@@ -0,0 +1,18 @@
+
+
+number = 10
+print(number)
+
+num2 = 5
+print(number+num2)
+
+sum = number + num2
+print("sum number")
+print(sum)
+
+name = "Ishani Ganguly"
+print(name)
+
+number=10
+number=25
+print(25)
